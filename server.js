16265883f8359c02624ce9dbe7ebcd6725556a6a
gnsoft <mobileteamdeveloper@gmail.com>
2016-09-05 19:18:00 +0700

    Update3

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-//var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    //ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+//var port = process.env.PORT || 3000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 server.listen(port, function () {
 	console.log('Server listening at port %d', port);
